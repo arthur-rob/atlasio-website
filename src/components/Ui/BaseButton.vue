7320371b142b6f6def70bb48e884481d42edd0b7
Arthur Rob <arthur.rob.dev@gmail.com>
2025-07-17 10:53:42 +0200

    core(button): improve css hover status

--- src/components/Ui/BaseButton.vue
+++ src/components/Ui/BaseButton.vue
@@ -1,9 +1,9 @@
 <template>
     <button
-        class="cursor-pointer flex items-center h-10 base-gradient-bg p-[2px] rounded-md outline-gray-200 active:outline-2 transition-outline duration-100"
+        class="cursor-pointer flex items-center h-10 base-gradient-bg p-[2px] rounded-md outline-gray-200 active:outline-2 transition-outline duration-100 hover:drop-shadow-sm transition-drop-shadow"
         :class="{ 'is-loading': loading, disabled: disabled || loading }"
     >
-        <div class="px-4 py-1 bg-white rounded h-full flex items-center">
+        <div class="px-4 py-1 bg-white rounded h-full flex items-center hover:bg-stone-50 duration-300 transition-colors">
             <span v-if="prependIcon" class="material-symbols-outlined mr-2">
                 {{ prependIcon }}</span
             >
